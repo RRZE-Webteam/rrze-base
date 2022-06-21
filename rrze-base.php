9a141150cd88faa15a01a3d7063638a19bfd3714
Rolf <rolf.v.d.forst@fau.de>
2022-06-21 16:26:54 +0200

    Update rrze-base.php

--- rrze-base.php
+++ rrze-base.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Base
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-base
 Description:     Base template for WordPress plugins.
-Version:         0.1.1
+Version:         0.1.2
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
@@ -18,7 +18,7 @@ namespace RRZE\Base;
 defined('ABSPATH') || exit;
 
 const RRZE_PHP_VERSION = '7.4';
-const RRZE_WP_VERSION  = '5.6';
+const RRZE_WP_VERSION  = '6.0';
 
 /**
  * Composer autoload
@@ -32,7 +32,7 @@ require_once __DIR__ . '/vendor/autoload.php';
  */
 spl_autoload_register(function ($class) {
     $prefix = __NAMESPACE__;
-    $base_dir = __DIR__ . '/includes/';
+    $baseDir = __DIR__ . '/includes/';
 
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
@@ -40,7 +40,7 @@ spl_autoload_register(function ($class) {
     }
 
     $relativeClass = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relativeClass) . '.php';
+    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
     if (file_exists($file)) {
         require $file;
@@ -54,38 +54,35 @@ register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
 /**
- * Load textdomain.
+ * Loads a plugin’s translated strings.
  */
-function load_textdomain()
+function loadTextdomain()
 {
-    load_plugin_textdomain(
-        'rrze-base',
-        false,
-        sprintf('%s/languages/', dirname(plugin_basename(__FILE__)))
-    );
+    load_plugin_textdomain('rrze-base', false, dirname(plugin_basename(__FILE__)) . '/languages');
 }
 
 /**
- * System requirement verification.
+ * System requirements verification.
+ * @return string Return an error message.
  */
-function system_requirements()
+function systemRequirements(): string
 {
-    load_textdomain();
     $error = '';
     if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
         $error = sprintf(
-            __('The server is running PHP version %s. The Plugin requires at least PHP version %s.', 'rrze-base'),
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-base'),
             PHP_VERSION,
             RRZE_PHP_VERSION
         );
     } elseif (version_compare($GLOBALS['wp_version'], RRZE_WP_VERSION, '<')) {
         $error = sprintf(
-            __('The server is running WordPress version %s. The Plugin requires at least WordPress version %s.', 'rrze-base'),
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-base'),
             $GLOBALS['wp_version'],
             RRZE_WP_VERSION
         );
     }
-
     return $error;
 }
 
@@ -94,10 +91,17 @@ function system_requirements()
  */
 function activation()
 {
-    $error = system_requirements();
-    if ($error) {
+    loadTextdomain();
+    if ($error = systemRequirements()) {
         deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(sprintf(__('Plugins: %s: %s', 'rrze-base'), plugin_basename(__FILE__), $error));
+        wp_die(
+            sprintf(
+                /* translators: 1: The plugin name, 2: The error string. */
+                __('Plugins: %1$s: %2$s', 'rrze-base'),
+                plugin_basename(__FILE__),
+                $error
+            )
+        );
     }
 }
 
@@ -119,29 +123,29 @@ function plugin()
     if (null === $instance) {
         $instance = new \RRZE\WP\Plugin(__FILE__);
     }
-
     return $instance;
 }
 
 /**
- * Loaded callback function.
- *
+ * Execute on 'plugins_loaded' API/action.
  * @return void
  */
 function loaded()
 {
-    add_action('init', __NAMESPACE__ . '\load_textdomain');
+    loadTextdomain();
     plugin()->loaded();
-    $error = system_requirements();
-    if ($error) {
+    if ($error = systemRequirements()) {
         add_action('admin_init', function () use ($error) {
             if (current_user_can('activate_plugins')) {
-                $pluginData = get_plugin_data(plugin()->get_file());
+                $pluginData = get_plugin_data(plugin()->getFile());
                 $pluginName = $pluginData['Name'];
-                $tag = is_plugin_active_for_network(plugin()->get_basename()) ? 'network_admin_notices' : 'admin_notices';
+                $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
                 add_action($tag, function () use ($pluginName, $error) {
                     printf(
-                        '<div class="notice notice-error"><p>' . __('Plugins: %s: %s', 'rrze-base') . '</p></div>',
+                        '<div class="notice notice-error"><p>' .
+                            /* translators: 1: The plugin name, 2: The error string. */
+                            __('Plugins: %1$s: %2$s', 'rrze-base') .
+                            '</p></div>',
                         esc_html($pluginName),
                         esc_html($error)
                     );
